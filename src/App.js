bd4c21ceaa98a77a617593dcafe0a76cc0ead07b
Swarali Manik More <swaralimore3@gamil.com>
2025-06-12 16:24:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+
+import First from './First';
+
+function App() {
+  return (
+    <div className="app">
+     <h1>mynany</h1>
+      <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/First.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div>
+      <h1>Hello Darling....this code is happens without any eroor</h1>
+    </div>
+  )
+}
+
+export default First
